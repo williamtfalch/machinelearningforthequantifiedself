dd971a2b3930c355a2cb886a8f66f5f06fb7a1dc
William Tallis Falch <william.tfalch@gmail.com>
2019-06-06 14:47:13 +0200

    did the non-visualization part (I think)

---
+++ assignment1/3-2.py
@@ -0,0 +1,37 @@
+from numpy import mean, std
+from matplotlib import pyplot as plt
+import pandas as pd
+
+from CreateDataset import CreateDataset
+from OutlierDetection import DistributionBasedOutlierDetection, DistanceBasedOutlierDetection
+from VisualizeDataset import VisualizeDataset
+
+dataset = CreateDataset("./", 1000)
+distribution_bod = DistributionBasedOutlierDetection()
+distance_bod = DistanceBasedOutlierDetection()
+vis = VisualizeDataset()
+
+dataset.add_numerical_dataset(
+    "chapter2_result.csv", "timestamps", ["acc_phone_x"])
+
+chauvs = {}
+mixes = {}
+simples = {}
+localoutliers = {}
+
+data_table = dataset.data_table
+
+for i in range(5):
+    c = 1 + i*0.5
+    chauvs[i] = distribution_bod.chauvenet(data_table, "acc_phone_x", c=c)
+    mixes[i] = distribution_bod.mixture_model(data_table, "acc_phone_x")
+
+    simples[i] = distance_bod.simple_distance_based(
+        data_table, ["acc_phone_x"], "euclidian", c, 1/c)
+    localoutliers[i] = distance_bod.local_outlier_factor(
+        data_table, ["acc_phone_x", "euclidian", 2*c])
+
+    #vis.plot_dataset(chauvs[i], ["x"])
+
+    # heart_rates = list(map(lambda v: (v[0], v[1]), list(pd.read_csv(
+    #    "../crowdsignals/heart_rate_smartwatch.csv", usecols=[2, 3]).values)))
--- assignment1/3-3.py
+++ assignment1/3-3.py
@@ -3,24 +3,12 @@ from matplotlib import pyplot as plt
 import pandas as pd
 
 from CreateDataset import CreateDataset
-from OutlierDetection import DistributionBasedOutlierDetection, DistanceBasedOutlierDetection
 from VisualizeDataset import VisualizeDataset
 
-acc_x_dataset = CreateDataset("../crowdsignals/", 1000)
-dbod = DistributionBasedOutlierDetection()
+dataset = CreateDataset("./", 1000)
 vis = VisualizeDataset()
 
-acc_x_dataset.add_numerical_dataset(
-    "accelerometer_phone_tiny.csv", "timestamps", ["x"])
+dataset.add_numerical_dataset(
+    "chapter2_result.csv", "timestamps", ["hr_watch_rate"])
 
-
-chauvs = {}
-for i in range(5):
-    c = 1 + i*0.5
-    chauvs[i] = dbod.chauvenet(acc_x_dataset.data_table, "x", c=c)
-    # print(chauvs[i])
-    vis.plot_dataset(chauvs[i], ["x"])
-
-
-# heart_rates = list(map(lambda v: (v[0], v[1]), list(pd.read_csv(
-#    "../crowdsignals/heart_rate_smartwatch.csv", usecols=[2, 3]).values)))
+data_table = dataset.data_table
